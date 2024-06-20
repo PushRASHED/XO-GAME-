f6ad1dbe49d1631c5c6a46e08f3c32a65d2ef4c1
PushRASHED <166936286+PushRASHED@users.noreply.github.com>
2024-06-20 22:00:29 +0300

    fix a winner function problem

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 let title = document.querySelector('.title')
-let turn = 'X';
- 
+let turn = 'x';
+let squares =[];
+
+function winner(){
+for(let i = 1; i<10;i++){
+  squares[i]= document.getElementById('item' + i).innerHTML;
+
+}
+if(squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != ''){
+    console.log('winner')
+}
+
+}
 
 
 
  
 function game(id){
 let element = document.getElementById(id)
-if(turn === 'X' && element.innerHTML == ""){
-    element.innerHTML = "X"
-    turn = 'O'
-    title.innerHTML = 'O '
+if(turn === 'x' && element.innerHTML == ''){
+    element.innerHTML = 'x'
+    turn = 'o'
+    title.innerHTML = 'o'
 }
-else if(turn === 'O' && element.innerHTML == ""){
+else if(turn === 'o' && element.innerHTML == ""){
 
-    element.innerHTML = "O"
-    turn = 'X'
-    title.innerHTML = 'X '
+    element.innerHTML = 'o'
+    turn = 'x'
+    title.innerHTML = 'x'
 
 }
 
+winner();
 }
