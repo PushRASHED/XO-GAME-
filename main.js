8e9550bdcdc87ccaf6bcefb8f241bf3b8f9a0866
PushRASHED <166936286+PushRASHED@users.noreply.github.com>
2024-06-20 22:18:44 +0300

    put every singal prospect to win in XO

--- main.js
+++ main.js
@@ -7,9 +7,51 @@ for(let i = 1; i<10;i++){
   squares[i]= document.getElementById('item' + i).innerHTML;
 
 }
-if(squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != ''){
-    console.log('winner')
+// vertical Prospect
+ if(squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != ''){
+
 }
+
+else if(squares[4] == squares[5] && squares[5] == squares[6] && squares[5] != ''){
+
+}
+
+else if(squares[7] == squares[8] && squares[8] == squares[9] && squares[7] != ''){
+
+}
+
+// horziantial Prospect
+else if(squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != ''){
+
+}
+
+else if(squares[2] == squares[5] && squares[5] == squares[8] && squares[8] != ''){
+
+}
+
+else if(squares[3] == squares[6] && squares[6] == squares[9] && squares[9] != ''){
+
+}
+
+// cross Prospect
+else if(squares[3] == squares[5] && squares[5] == squares[7] && squares[7] != ''){
+
+}
+
+else if(squares[1] == squares[5] && squares[5] == squares[9] && squares[9] != ''){
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
